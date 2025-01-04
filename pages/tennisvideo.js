0459c4292219bc9d847c31b8d02cffcd905009b6
ragnamarie <ragna.steinboemer@gmail.com>
2025-01-04 18:16:07 +0100

    sound added

--- pages/tennisvideo.js
+++ pages/tennisvideo.js
@@ -1,7 +1,38 @@
+import { useState, useEffect } from "react";
 import TennisVideo from "@/Components/TennisVideo";
 import TennisVideoMobile from "@/Components/TennisVideoMobile";
 
 export default function TennisPageWithVideo() {
+  const [isAudioPlaying, setIsAudioPlaying] = useState(false);
+  const [audioInstance, setAudioInstance] = useState(null);
+
+  useEffect(() => {
+    const audio = new Audio("/music.mp3");
+    audio.loop = true; // Set audio to loop
+    setAudioInstance(audio);
+
+    // Clean up the audio instance when the component unmounts
+    return () => {
+      if (audio) {
+        audio.pause();
+        audio.src = ""; // Clear the audio source to free up resources
+      }
+    };
+  }, []);
+
+  const toggleAudio = () => {
+    if (audioInstance) {
+      if (isAudioPlaying) {
+        audioInstance.pause();
+      } else {
+        audioInstance
+          .play()
+          .catch((error) => console.error("Audio playback failed:", error));
+      }
+      setIsAudioPlaying(!isAudioPlaying);
+    }
+  };
+
   return (
     <>
       <div className="desktop-only">
@@ -9,6 +40,11 @@ export default function TennisPageWithVideo() {
       </div>
       <div className="mobile-only">
         <TennisVideoMobile />
+      </div>
+
+      {/* Sound toggle */}
+      <div className="sound-toggle" onClick={toggleAudio}>
+        {isAudioPlaying ? "SOUND OFF" : "SOUND ON"}
       </div>
 
       <style jsx>{`
@@ -36,6 +72,23 @@ export default function TennisPageWithVideo() {
           .mobile-only {
             display: none;
           }
+        }
+
+        .sound-toggle {
+          position: fixed;
+          bottom: 20px;
+          right: 20px;
+          color: white;
+          padding: 10px 20px;
+          border-radius: 5px;
+          font-size: 12px;
+          cursor: pointer;
+          z-index: 1000;
+          user-select: none;
+        }
+
+        .sound-toggle:hover {
+          color: black;
         }
       `}</style>
     </>
